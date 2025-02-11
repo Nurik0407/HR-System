d12571a5d25830bf63f8b28d756ff9a4695dc416
nrk <116573469+Nurik0407@users.noreply.github.com>
2025-02-11 14:58:20 +0600

    initial

---
+++ src/main/java/com/example/border/BorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.border;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderApplication.class, args);
+	}
+
+}
