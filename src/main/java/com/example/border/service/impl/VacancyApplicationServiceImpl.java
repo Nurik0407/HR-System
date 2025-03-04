e7ef9ab31b44013e4563c7324b56c71db2196471
nrk <116573469+Nurik0407@users.noreply.github.com>
2025-03-04 12:43:32 +0600

    add default sort by createdAt desc

--- src/main/java/com/example/border/service/impl/VacancyApplicationServiceImpl.java
+++ src/main/java/com/example/border/service/impl/VacancyApplicationServiceImpl.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -118,7 +119,7 @@ public class VacancyApplicationServiceImpl implements VacancyApplicationService 
         log.info("Fetching applications for applicant: {}, page: {}, size: {}, keyword: {}, status: {}, dateFilter: {}",
                 currentApplicant.getId(), page, size, keyword, status, dateFilter);
 
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
 
         Specification<VacancyApplication> specification = buildSpecificationApplication(
                 keyword, status, dateFilter, currentApplicant.getId()
